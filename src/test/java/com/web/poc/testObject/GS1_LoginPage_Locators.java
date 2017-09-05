2bb18c6aa7f357a6c287023b244397e4cdaecc6b
bmsyamkumar <30311414+bmsyamkumar@users.noreply.github.com>
2017-09-05 11:40:29 +0530

    test commit

--- src/test/java/com/web/poc/testObject/GS1_LoginPage_Locators.java
+++ src/test/java/com/web/poc/testObject/GS1_LoginPage_Locators.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 public class GS1_LoginPage_Locators{
 	public static By userName_txt=By.id("user");
 	public static By password_txt=By.id("password");	
-	public static By login_btn=By.xpath("//button[@type='login']");  
+	public static By login_btn=By.xpath("//button[@type='submit']");  
 
 }
 	
